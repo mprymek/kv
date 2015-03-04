a9e239642537ab1bc75ed210a7112ec7a5f9942e
Issac Trotts <issac.trotts@gmail.com>
2015-03-03 16:31:24 -0800

    Convert tabs to spaces

--- lib/kv/supervisor.ex
+++ lib/kv/supervisor.ex
@@ -1,22 +1,22 @@
 defmodule KV.BucketAndRegistrySupervisor do
-	use Supervisor
+    use Supervisor
 
     @manager_name KV.EventManager
     @registry_name KV.Registry
     @bucket_sup_name KV.Bucket.Supervisor
-	
-	def start_link(opts \\ []) do
-		Supervisor.start_link(__MODULE__, :ok, opts)
-	end
-	
-	def init(:ok) do
-		children = [
-	        supervisor(KV.Bucket.Supervisor, [[name: @bucket_sup_name]]),
-	        worker(KV.Registry, [@manager_name, [name: @registry_name]])
-		]
-		
-		supervise(children, strategy: :one_for_all)
-	end
+    
+    def start_link(opts \\ []) do
+        Supervisor.start_link(__MODULE__, :ok, opts)
+    end
+    
+    def init(:ok) do
+        children = [
+            supervisor(KV.Bucket.Supervisor, [[name: @bucket_sup_name]]),
+            worker(KV.Registry, [@manager_name, [name: @registry_name]])
+        ]
+        
+        supervise(children, strategy: :one_for_all)
+    end
 end
 
 defmodule KV.Bucket.Supervisor do
@@ -49,23 +49,23 @@ defmodule KV.Supervisor do
     @manager_name KV.EventManager
     @registry_name KV.Registry
     @bucket_sup_name KV.Bucket.Supervisor
-	@bucket_and_reg_sup_name KV.BucketAndRegistrySupervisor
+    @bucket_and_reg_sup_name KV.BucketAndRegistrySupervisor
 
     def init(:ok) do
         IO.puts "Starting KV.Supervisor"
         children = [
             worker(GenEvent, [[name: @manager_name]]),
-#			supervisor(KV.BucketAndRegistrySupervisor, [[name: @bucket_and_reg_sup_name]])
+#            supervisor(KV.BucketAndRegistrySupervisor, [[name: @bucket_and_reg_sup_name]])
             supervisor(KV.Bucket.Supervisor, [[name: @bucket_sup_name]]),
             worker(KV.Registry, [@manager_name, [name: @registry_name]])
-		]
+        ]
         IO.puts "Supervising children: #{inspect children}"
         supervise(children, strategy: :one_for_one)
     end
-	
-	def get_buckets_registry(sup) do
-		[_mgr, _buckets_sup, reg] = Supervisor.which_children(sup)
-		reg
-	end
+    
+    def get_buckets_registry(sup) do
+        [_mgr, _buckets_sup, reg] = Supervisor.which_children(sup)
+        reg
+    end
 end
 
